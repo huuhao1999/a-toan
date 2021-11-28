d7c4bb4fc6e77fed631c49aaf62cd94a6cac1faa
Huu Hao <huuhao1999@gmail.com>
2021-11-28 09:34:19 +0700

    cd

--- app.js
+++ app.js
@@ -10,13 +10,17 @@
 // });
 
 
-$('#input-transfer-amount').on('blur', function() {
+$('#input-transfer-amount').on('blur', function () {
+  console.log(this.value.replace(/,/g, ''));
   const value = this.value.replace(/,/g, '');
-  this.value = parseFloat(value).toLocaleString('en-US', {
+  let avalue = parseFloat(value).toLocaleString('en-US', {
     style: 'decimal',
     maximumFractionDigits: 2,
     minimumFractionDigits: 0
-  })+'đ';
+  }) + 'đ';
+  document.getElementById('input-transfer-amount1').value = avalue;
+  document.getElementById('input-transfer-amount').style.display = 'none';
+  document.getElementById('input-transfer-amount1').style.display = '';
 });
 
 // function formatNumber(n) {
@@ -28,19 +32,19 @@ $('#input-transfer-amount').on('blur', function() {
 // function formatCurrency(input, blur) {
 //   // appends $ to value, validates decimal side
 //   // and puts cursor back in right position.
-  
+
 //   // get input value
 //   var input_val = input.val();
-  
+
 //   // don't validate empty input
 //   if (input_val === "") { return; }
-  
+
 //   // original length
 //   var original_len = input_val.length;
 
 //   // initial caret position 
 //   var caret_pos = input.prop("selectionStart");
-    
+
 //   // check for decimal
 //   if (input_val.indexOf(".") >= 0) {
 
@@ -58,12 +62,12 @@ $('#input-transfer-amount').on('blur', function() {
 
 //     // validate right side
 //     right_side = formatNumber(right_side);
-    
+
 //     // On blur make sure 2 numbers after decimal
 //     if (blur === "blur") {
 //       right_side += "00";
 //     }
-    
+
 //     // Limit decimal to only 2 digits
 //     right_side = right_side.substring(0, 2);
 
@@ -77,7 +81,7 @@ $('#input-transfer-amount').on('blur', function() {
 //     input_val = formatNumber(input_val);
 //     input_val = input_val+"đ";
 //   }
-  
+
 //   // send updated string to input
 //   input.val(input_val);
 
@@ -96,24 +100,24 @@ $('#input-transfer-amount').on('blur', function() {
 
 let form = document.getElementById("form-qr");
 form.addEventListener("keydown", (event) => {
-    let key = event.key;
-    if (key == "Enter") {
-     
-        let activeElm = document.activeElement;
-        let content = document.getElementById("input-transfer-content").value;
-        let amount = document.getElementById("input-transfer-amount").value;
-        document.getElementById("input-transfer-amount").pattern = "*";
-        console.log(amount);
-        if (amount) {
-            amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
-        }
-        console.log(amount);
-       
-        if (activeElm == content && content.value != "" && amount.value == "") {
-            amount.focus();
-            event.preventDefault();
-        }
+  let key = event.key;
+  if (key == "Enter") {
+
+    let activeElm = document.activeElement;
+    let content = document.getElementById("input-transfer-content").value;
+    let amount = document.getElementById("input-transfer-amount").value;
+    document.getElementById("input-transfer-amount").pattern = "*";
+    console.log(amount);
+    if (amount) {
+      amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
     }
+    console.log(amount);
+
+    if (activeElm == content && content.value != "" && amount.value == "") {
+      amount.focus();
+      event.preventDefault();
+    }
+  }
 });
 // document.getElementById("input-transfer-amount").onblur = function (event) {
 //     if (!event?.relatedTarget?.id) {
@@ -128,43 +132,42 @@ form.addEventListener("keydown", (event) => {
 
 // }
 form.addEventListener("submit", (event) => {
-    console.log('vo nhanh 2');
-    event.preventDefault();
-    let bankName = "Vietcombank";
-    let bankNumber = "1111514131";
-    let accountName = "Vũ Đình Toàn";
-    let content = document.getElementById("input-transfer-content").value;
-    let amount = document.getElementById("input-transfer-amount").value;
-    if (amount) {
-        amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
-    }
-    if (content != "" && amount != "") {
-        let img = document.getElementById("qr-image");
-        let imageUrl = `https://api.huuhao.club/${bankName}/${bankNumber}/${amount}/${encodeURI(content)}/qr_only_beck-company.png?accountName=${encodeURI(accountName)}`;
-        console.log(imageUrl);
-        img.src = imageUrl;
-        document.getElementById("input-transfer-amount").pattern = "^\$\d{1,3}(,\d{3})*(\.\d+)?$";
-    }
+  console.log('vo nhanh 2');
+  event.preventDefault();
+  let bankName = "Vietcombank";
+  let bankNumber = "1111514131";
+  let accountName = "Vũ Đình Toàn";
+  let content = document.getElementById("input-transfer-content").value;
+  let amount = document.getElementById("input-transfer-amount").value;
+  if (amount) {
+    amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
+  }
+  if (content != "" && amount != "") {
+    let img = document.getElementById("qr-image");
+    let imageUrl = `https://api.vietqr.io/${bankName}/${bankNumber}/${amount}/${encodeURI(content)}/qr_only_beck-company.png?accountName=${encodeURI(accountName)}`;
+    console.log(imageUrl);
+    img.src = imageUrl;
+    document.getElementById("input-transfer-amount").pattern = "^\$\d{1,3}(,\d{3})*(\.\d+)?$";
+  }
 });
 
 
 document.getElementById("field__submit").onclick = function (event) {
-    let content = document.getElementById("input-transfer-content").value;
-    let amount = document.getElementById("input-transfer-amount").value;
-    document.getElementById("input-transfer-amount").pattern = "*";
-    console.log(amount);
-    if (amount) {
-        amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
-    }
-    console.log(amount);
-   
-    if ( content.value != "" && amount.value == "") {
-        amount.focus();
-        event.preventDefault();
-    }
+  let content = document.getElementById("input-transfer-content").value;
+  let amount = document.getElementById("input-transfer-amount").value;
+  document.getElementById("input-transfer-amount").pattern = "*";
+  console.log(amount);
+  if (amount) {
+    amount = Number((amount.toString()).replace('đ', '').split(',').join(''));
+  }
+  console.log(amount);
+
+  if (content.value != "" && amount.value == "") {
+    amount.focus();
+    event.preventDefault();
+  }
 }
-// document.getElementById("input-transfer-amount").onclick = function () {
-//     this.value = Number((this.value).replace('đ', '').split(',').join(''));
-//     this.type = 'number';
-//     // console.log('csdcsdcsd');
-// }
+document.getElementById("input-transfer-amount1").onclick = function () {
+  document.getElementById('input-transfer-amount').style.display = '';
+  document.getElementById('input-transfer-amount1').style.display = 'none';
+}
